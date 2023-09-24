343ef870d2c489fceeb7e9093a52c13695e12e52
szentmartonylaszlo <szentmartonylaszlo@gmail.com>
2023-09-24 13:43:46 +0200

    hibák jelölése

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int* b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-        for (int i = 0;)
-        {
-            b[i] = i * 2;
-        }
-    for (int i = 0; i; i++)
+    int* b = new int[NELEMENTS]; // változónév hiba
+    std::cout << '1-100 ertekek duplazasa' // '' helyett ""
+    for (int i = 0;) // ciklus szintaxis hiba
     {
-        std::cout << "Ertek:"
+        b[i] = i * 2;
+    }
+    for (int i = 0; i; i++) // ciklus szintaxis hiba
+    {
+        std::cout << "Ertek:" // érték kiírása hiányzik
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // nincs megadva kezdõérték
+    for (int i = 0; i < N_ELEMENTS, i++) // ciklus szintaxis hiba
     {
-        atlag += b[i]
+        atlag += b[i] // ; hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    // nincs felszabadítva a memória
     return 0;
 }
